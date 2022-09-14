75c9c07acb0232d3a2be1dfd43859171a37b9d1b
shravandeveloper <shravanpatil41@gmail.comgit config -listgit config --global user.email shravanpatil41@gmail.com>
2022-09-14 19:26:22 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,76 @@
+
+$('document').ready(function(){
+
+// slider
+$('.responsive').slick({
+  dots: true,
+  infinite: true,
+  arrow:true,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+// Drop down menu
+$('.nav-element .dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
+
+//Mute and Unmute Video code
+$(".sound").click(function(){
+  $('img',this).toggle();
+
+  if( $("#bgvid").prop('muted') ) {
+          $("#bgvid").prop('muted', false);
+    } else {
+      $("#bgvid").prop('muted', true);
+    }
+});
+
+
+// Video play and pause button
+$('.moreinfo').click(function(){
+  $('#bgvid').trigger('pause');
+  
+})
+$('.btn-close').click(function(){
+  $('#bgvid').trigger('play');
+})
+
+
+})
+
+
+
