3c3c7933281ccdf2d80c0cfc48bcdfb720215512
shravandeveloper <shravanpatil41@gmail.comgit config -listgit config --global user.email shravanpatil41@gmail.com>
2022-10-26 00:39:33 +0530

    commit

--- js/app.js
+++ js/app.js
@@ -19,6 +19,36 @@ switchmode.onclick = function(){
   body.classList.toggle('dark');
 }
 
+// Geolocation code
+
+let x = document.getElementById('mylocation');
+
+function geolocation(){
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showPosition)
+  }
+  else {
+    x.innerText = "Geo not supported"
+  }
+}
+function showPosition(data){
+  console.log(data);
+    let latitude = data.coords.latitude;
+    let longitude = data.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
+
+    // api calling
+    fetch(url, { method: `GET` })
+        // below .then will return promise
+        .then((res) => res.json())          // .then is called promise 
+        // below .then will resolve the promise; return data
+        .then((data) => {
+            let weatherData = data.list[0].temp.day;
+            x.innerHTML = `${weatherData}\u00B0C`;     //    \u00B0 is code for degree symbol
+        });
+}
+
+geolocation();
 
 
 $('document').ready(function(){
